1b630130ce71b824dfb592bcc0554ed7cc972d40
Laurent MacKay <lmackay@localhost.localdomain>
2024-06-25 10:46:01 -0700

    minor housekeeping

--- ResearchTools/Dict.py
+++ ResearchTools/Dict.py
@@ -5,7 +5,7 @@ import json
 import os
 from typing import Any, Dict
 import numpy as np
-from VertexTissue.Iterable import first_item
+from .Iterable import first_item
 
 def dict_product(d):
     '''
